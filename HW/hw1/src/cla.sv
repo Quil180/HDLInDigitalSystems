a5b94c941cc9988a0d46844e0c22a38a0ec5aacc
Quil <quil180gaming@gmail.com>
2026-02-20 17:34:32 -0500

    finished hw1

--- HW/hw1/src/cla.sv
+++ HW/hw1/src/cla.sv
@@ -10,25 +10,25 @@ module cla (
 
   // create the generate and propagate and buffers for carries
   wire [3:0] Generate;
-  wire [3:0] Propogate;
+  wire [3:0] Propagate;
   wire temp_carry[2:0];
 
   // define the logic for the generate and propagate
   assign Generate = A & B;
-  assign Propogate = A ^ B;
+  assign Propagate = A ^ B;
 
   // define the logic for the buffer carries and output carry
-  assign temp_carry[0] = Generate[0] | (Propogate[0] & Cin);
-  assign temp_carry[1] = Generate[1] | (Propogate[1] & Generate[0]) | (Propogate[1] & Propogate[0] & Cin);
-  assign temp_carry[2] = Generate[2] | (Propogate[2] & Generate[1]) | (Propogate[2] & Propogate[1] & Generate[0]) | (Propogate[2] & Propogate[1] & Propogate[0] & Cin);
-  assign Cout = Generate[3] | (Propogate[3] & Generate[2]) | (Propogate[3] & Propogate[2] & Generate[1]) |
-(Propogate[3] & Propogate[2] & Propogate[1] & Generate[0]) | (Propogate[3] & Propogate[2] & Propogate[1] & Propogate[0] & Cin);
+  assign temp_carry[0] = Generate[0] | (Propagate[0] & Cin);
+  assign temp_carry[1] = Generate[1] | (Propagate[1] & Generate[0]) | (Propagate[1] & Propagate[0] & Cin);
+  assign temp_carry[2] = Generate[2] | (Propagate[2] & Generate[1]) | (Propagate[2] & Propagate[1] & Generate[0]) | (Propagate[2] & Propagate[1] & Propagate[0] & Cin);
+  assign Cout = Generate[3] | (Propagate[3] & Generate[2]) | (Propagate[3] & Propagate[2] & Generate[1]) |
+(Propagate[3] & Propagate[2] & Propagate[1] & Generate[0]) | (Propagate[3] & Propagate[2] & Propagate[1] & Propagate[0] & Cin);
 
   // defining the summation logic
-  assign S[0] = Propogate[0] ^ Cin;
-  assign S[1] = Propogate[1] ^ temp_carry[0];
-  assign S[2] = Propogate[2] ^ temp_carry[1];
-  assign S[3] = Propogate[3] ^ temp_carry[2];
+  assign S[0] = Propagate[0] ^ Cin;
+  assign S[1] = Propagate[1] ^ temp_carry[0];
+  assign S[2] = Propagate[2] ^ temp_carry[1];
+  assign S[3] = Propagate[3] ^ temp_carry[2];
 
 endmodule
 
